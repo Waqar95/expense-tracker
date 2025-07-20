a18b9d1d7281e92fe92b2028ab7b80c2c7ad6f6e
waqar95 <waqar95ahmed@gmail.com>
2025-07-20 12:15:24 +0200

    Built Add Expense form with validation and service connection

--- src/app/component/add-expense/add-expense.component.ts
+++ src/app/component/add-expense/add-expense.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ExpenseService } from '../../services/expense.service';
+import { Expense } from '../../models/expense.model';
 
 @Component({
   selector: 'app-add-expense',
-  imports: [],
   templateUrl: './add-expense.component.html',
-  styleUrl: './add-expense.component.css'
+  styleUrls: ['./add-expense.component.css'],
 })
 export class AddExpenseComponent {
+  expenseForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private expenseService: ExpenseService) {
+    this.expenseForm = this.fb.group({
+      title: ['', Validators.required],
+      amount: [0, [Validators.required, Validators.min(0.01)]],
+      category: ['', Validators.required],
+      date: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.expenseForm.valid) {
+      const newExpense: Expense = {
+        id: Date.now(),
+        ...this.expenseForm.value,
+        date: new Date(this.expenseForm.value.date),
+      };
+      this.expenseService.addExpense(newExpense);
+      this.expenseForm.reset();
+    }
+  }
 }
